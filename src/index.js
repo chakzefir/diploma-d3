4aa8db34e9f1e39a6b381fb25565b838086b0ca5
Sergey Chikirev <sergey.chikirev@team.wrike.com>
2019-05-25 19:54:57 +0300

    Add initialData generator

--- src/index.js
+++ src/index.js
@@ -5,43 +5,30 @@ function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'Bachelor'], ' ');
 
     return element;
 }
 
-const forceData = {
-    'links': [
-        {
-            source: 'Client1',
-            target: 'Server',
-            value: 1,
-        }, {
-            source: 'Client2',
-            target: 'Server',
-            value: 1,
-        }, {
-            source: 'Client3',
-            target: 'Server',
-            value: 2,
-        }
-    ],
-    'nodes': [
-        {
-            id: 'Server',
-            group: 1,
-        }, {
-            id: 'Client1',
-            group: 2,
-        }, {
-            id: 'Client2',
-            group: 2,
-        }, {
-            id: 'Client3',
-            group: 2,
-        }
-    ]
-};
+const initialForceData = { 'links': [ { source: 'Client1', target: 'Server', value: 1, }, { source: 'Client2', target: 'Server', value: 1, }, { source: 'Client3', target: 'Server', value: 2, } ], 'nodes': [ { id: 'Server', group: 1, }, { id: 'Client1', group: 2, }, { id: 'Client2', group: 2, }, { id: 'Client3', group: 2, } ] };
+
+function dataGenerator(qty = 3, topology = 'star') {
+    let linksArray = [];
+    let nodesArray = [{id: 'Server', group: 1}];
+
+    for(let i = 0; i < qty; i++) {
+        let currentTarget = i === 0 || topology === 'star' ? 'Server' : `Client${Number(i-1)}`;
+
+        linksArray.push({source: `Client${i}`,target: currentTarget})
+        nodesArray.push({id: `Client${i}`, group: 2})
+    }
+
+    return {
+        'links': linksArray,
+        'nodes': nodesArray
+    }
+}
+
 
 document.body.appendChild(component());
-const force = new Force(forceData);
+const force = new Force(dataGenerator(5));
